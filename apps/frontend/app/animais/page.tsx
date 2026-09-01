01ed276516524676c3dc2e27f6b078d3d8e65f1f
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2026-09-01 20:35:04 -0300

    Feat: Start structure frontend on the project

---
+++ apps/frontend/app/animais/page.tsx
@@ -0,0 +1,12 @@
+/* import Link from 'next/link';
+import { prisma } from '@/lib/prisma';
+import { deletePet } from './actions';
+
+export default async function PetsPage() {
+  const pets = await prisma.pets.findMany();
+    orderBy: { createdAt: 'desc' };
+})
+
+return (
+    <div>
+) */
